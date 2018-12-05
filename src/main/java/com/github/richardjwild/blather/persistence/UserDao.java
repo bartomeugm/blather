795eaf78aa8183486c52c57f6bedeca7a0fdc269
Bartomeu <bartomeugm@gmail.com>
2018-12-05 14:47:19 +0100

    Migrated find to use jdbcTemplate

--- src/main/java/com/github/richardjwild/blather/persistence/UserDao.java
+++ src/main/java/com/github/richardjwild/blather/persistence/UserDao.java
@@ -1,40 +1,56 @@
 package com.github.richardjwild.blather.persistence;
 
-import com.github.richardjwild.blather.user.User;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import java.sql.*;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+@EnableAutoConfiguration
 public class UserDao {
+    private DataSource dataSource;
     private Connection connection;
 
     public UserDao(Connection connection) {
         this.connection = connection;
     }
 
+    public UserDao(DriverManagerDataSource dataSource) {
+
+        this.dataSource = dataSource;
+    }
+
     public void saveUser(String name) {
-        PreparedStatement statement ;
+        PreparedStatement statement;
         try {
             statement = connection.prepareStatement("INSERT INTO users VALUES (?)");
             statement.setString(1, name);
             statement.execute();
         } catch (SQLException e) {
-            System.err.println("Saveuser failed: "+ e.getMessage());
+            System.err.println("Saveuser failed: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
     public String findUser(String name) {
-        PreparedStatement statement = null;
-        ResultSet results = null;
-        try {
-            statement = connection.prepareStatement("SELECT * FROM users WHERE name = ?");
-            statement.setString(1, name);
-            results = statement.executeQuery();
-            if(results.next()) return results.getString(1);
-        } catch (SQLException e) {
-            System.err.println("Finduser failed: "+ e.getMessage());
-            e.printStackTrace();
-        }
-        return null;
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        return jdbcTemplate.queryForObject("SELECT name FROM users WHERE name = ?", String.class, name);
+    }
+
+
+    public static void main(String[] args) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/blather?user=root&password=password");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password");
+
+        // Inject the datasource into the dao
+        UserDao dao = new UserDao(dataSource);
+        String username = dao.findUser("testuser");
+        System.out.println("Username: " + username);
     }
 }
