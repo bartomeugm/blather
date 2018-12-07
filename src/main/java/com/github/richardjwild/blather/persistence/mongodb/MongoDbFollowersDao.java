394d83b01579665921fa868413f9da00e8a84e95
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-12-07 15:56:39 +0000

    Finish implementation of MongoDbFollowersDao::saveFollowees

--- src/main/java/com/github/richardjwild/blather/persistence/mongodb/MongoDbFollowersDao.java
+++ src/main/java/com/github/richardjwild/blather/persistence/mongodb/MongoDbFollowersDao.java
@@ -2,18 +2,13 @@ package com.github.richardjwild.blather.persistence.mongodb;
 
 import com.github.richardjwild.blather.persistence.FollowersDao;
 import com.github.richardjwild.blather.user.User;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.set;
 
 public class MongoDbFollowersDao implements FollowersDao {
     private MongoCollection<Document> users;
@@ -24,32 +19,22 @@ public class MongoDbFollowersDao implements FollowersDao {
 
     @Override
     public void saveFollowees(String follower, Set<User> followees) {
+        List<String> followeesNames = new ArrayList<>();
+        followees.forEach(user -> followeesNames.add(user.name()));
+
         users.findOneAndUpdate(
-                eq("name",)
-        )
+                eq("name",follower),
+                set("followees", followeesNames)
+        );
     }
 
     @Override
     public Set<String> getFollowees(String follower) {
         Document user = users.find(eq("name", follower)).first();
-        List<String> followees = (List<String>) user.getOrDefault("followees", Collections.emptyList());
-        return new HashSet<>(followees);
-    }
-
-    public static void main(String[] args) {
-        MongoClient client = MongoClients.create();
-        MongoDatabase database = client.getDatabase("blather");
-        MongoCollection<Document> users = database.getCollection("users");
-
-        MongoDbFollowersDao followersDao = new MongoDbFollowersDao(users);
-
-        followersDao.saveFollowees("Richard", new HashSet<>(){{
-            add(new User("Sarah"));
-            add(new User("Jolene"));
-        }});
-
-        Set<String> followeesNames = followersDao.getFollowees("Richard");
-
-        followeesNames.forEach(System.out::println);
+        if (user != null) {
+            List<String> followees = (List<String>) user.getOrDefault("followees", Collections.EMPTY_LIST);
+            return new HashSet<>(followees);
+        }
+        return Collections.EMPTY_SET;
     }
 }
